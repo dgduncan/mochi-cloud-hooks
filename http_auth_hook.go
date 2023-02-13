015d82089db41dea57c456b90e3912c3b9c4baf9
Derek Duncan <dduncan@atlassian.com>
2023-02-13 15:57:34 +1100

    Add basic http-auth config

--- http_auth_hook.go
+++ http_auth_hook.go
@@ -2,13 +2,25 @@ package mochicloudhooks
 
 import (
 	"bytes"
+	"errors"
+	"net/http"
 
 	"github.com/mochi-co/mqtt/v2"
 	"github.com/mochi-co/mqtt/v2/packets"
 )
 
 type HTTPAuthHook struct {
+	httpclient     *http.Client
+	aclhost        string
+	clientauthhost string
+	superuserhost  string
 	mqtt.HookBase
+}
+
+type HTTPAuthHookConfig struct {
+	ACLHost                  string
+	SuperUserHost            string
+	ClientAuthenticationHost string
 }
 
 func (h *HTTPAuthHook) ID() string {
@@ -23,12 +35,31 @@ func (h *HTTPAuthHook) Provides(b byte) bool {
 }
 
 func (h *HTTPAuthHook) Init(config any) error {
+	authHookConfig, ok := config.(HTTPAuthHookConfig)
+	if !ok {
+		return errors.New("improper config")
+	}
+
+	h.aclhost = authHookConfig.ACLHost
+	h.clientauthhost = authHookConfig.ClientAuthenticationHost
+	h.superuserhost = authHookConfig.SuperUserHost
 	return nil
 }
 
 func (h *HTTPAuthHook) OnConnectAuthenticate(cl *mqtt.Client, pk packets.Packet) bool {
+	req, err := http.NewRequest(http.MethodGet, h.clientauthhost, http.NoBody)
+	if err != nil {
+		h.Log.Error().Err(err)
+		return false
+	}
 
-	return true
+	resp, err := h.httpclient.Do(req)
+	if err != nil {
+		h.Log.Error().Err(err)
+		return false
+	}
+
+	return resp.StatusCode >= 200 && resp.StatusCode < 300
 }
 
 func (h *HTTPAuthHook) OnACLCheck(cl *mqtt.Client, topic string, write bool) bool {

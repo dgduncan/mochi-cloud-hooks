2a4d7acfe9c34fc5785dd503f59c3309ce6b7472
Derek Duncan <dduncan@atlassian.com>
2023-02-21 09:49:16 +1100

    Add TestProvides hook

--- http_auth_hook_test.go
+++ http_auth_hook_test.go
@@ -3,6 +3,7 @@ package mochicloudhooks
 import (
 	"testing"
 
+	"github.com/mochi-co/mqtt/v2"
 	"github.com/stretchr/testify/require"
 )
 
@@ -10,4 +11,38 @@ func TestID(t *testing.T) {
 	authHook := new(HTTPAuthHook)
 
 	require.Equal(t, "http-auth-hook", authHook.ID())
+}
+
+func TestProvides(t *testing.T) {
+	authHook := new(HTTPAuthHook)
+
+	tests := []struct {
+		name           string
+		hook           byte
+		expectProvides bool
+	}{
+		{
+			name:           "Success - Provides OnACLCheck",
+			hook:           mqtt.OnACLCheck,
+			expectProvides: true,
+		},
+		{
+			name:           "Success - Provides OnConnectAuthenticate",
+			hook:           mqtt.OnConnectAuthenticate,
+			expectProvides: true,
+		},
+		{
+			name:           "Failure - Provides other hook",
+			hook:           mqtt.OnClientExpired,
+			expectProvides: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			require.Equal(t, tt.expectProvides, authHook.Provides(tt.hook))
+
+		})
+	}
 }

543c9ddda1f47b0ae008b2c04888d851ea35da41
Derek Duncan <dduncan@atlassian.com>
2023-02-21 10:20:48 +1100

    Add tests to http_auth_hook

--- http_auth_hook_test.go
+++ http_auth_hook_test.go
@@ -1,9 +1,14 @@
 package mochicloudhooks
 
 import (
+	"context"
+	"errors"
+	"net/http"
 	"testing"
 
+	gomock "github.com/golang/mock/gomock"
 	"github.com/mochi-co/mqtt/v2"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 )
 
@@ -42,6 +47,112 @@ func TestProvides(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 
 			require.Equal(t, tt.expectProvides, authHook.Provides(tt.hook))
+
+		})
+	}
+}
+
+func TestInit(t *testing.T) {
+	authHook := new(HTTPAuthHook)
+	authHook.Log = &zerolog.Logger{}
+
+	tests := []struct {
+		name        string
+		config      any
+		expectError bool
+	}{
+		{
+			name:        "Success - Proper config",
+			config:      HTTPAuthHookConfig{},
+			expectError: false,
+		},
+		{
+			name:        "Success - nil config",
+			config:      nil,
+			expectError: false,
+		},
+		{
+			name:        "Failure - improper config",
+			config:      "",
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			err := authHook.Init(tt.config)
+			if tt.expectError {
+				require.Error(t, err)
+			}
+
+		})
+	}
+}
+
+func TestOnConnectAuthenticate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockRT := NewMockRoundTripper(ctrl)
+
+	tests := []struct {
+		name   string
+		config any
+		mocks  func(ctx context.Context)
+
+		expectPass bool
+	}{
+		{
+			name: "Success - Proper config",
+			config: HTTPAuthHookConfig{
+				RoundTripper: mockRT,
+			},
+			expectPass: true,
+			mocks: func(ctx context.Context) {
+				mockRT.EXPECT().RoundTrip(gomock.Any()).Return(&http.Response{
+					StatusCode: http.StatusOK,
+				}, nil)
+
+			},
+		},
+		{
+			name: "Error - HTTP error",
+			config: HTTPAuthHookConfig{
+				RoundTripper: mockRT,
+			},
+			expectPass: false,
+			mocks: func(ctx context.Context) {
+				mockRT.EXPECT().RoundTrip(gomock.Any()).Return(nil, errors.New("Oh Crap"))
+			},
+		},
+		{
+			name: "Error - Non 2xx",
+			config: HTTPAuthHookConfig{
+				RoundTripper: mockRT,
+			},
+			expectPass: false,
+			mocks: func(ctx context.Context) {
+				mockRT.EXPECT().RoundTrip(gomock.Any()).Return(&http.Response{
+					StatusCode: http.StatusTeapot,
+				}, nil)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			tt.mocks(ctx)
+
+			authHook := new(HTTPAuthHook)
+			authHook.Log = &zerolog.Logger{}
+			authHook.Init(tt.config)
+
+			success := authHook.OnACLCheck(&mqtt.Client{}, "/topic", false)
+			require.Equal(t, tt.expectPass, success)
+			// if tt.expectError {
+			// 	require.Error(t, err)
+			// }
 
 		})
 	}
--- transport_test.go
+++ transport_test.go
@@ -8,32 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// func TestNewRoundTrip(t *testing.T) {
-
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
-
-// 	tests := []struct {
-// 		name      string
-// 		expectErr bool
-// 	}{
-// 		{
-// 			name:      "Success - Golden Path",
-// 			expectErr: false,
-// 		},
-// 	}
-
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-
-// 			req, _ := http.NewRequest("GET", "http://example.com", nil)
-// 			nt := NewTransport(http.DefaultTransport)
-
-// 			require.Nil(t, err)
-// 		})
-// 	}
-// }
-
 func TestRoundTrip(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
